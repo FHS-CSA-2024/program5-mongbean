c24eaf777c58db66952c6fd2d02bc4f8a4f93760
mongbean <112592782+mongbean@users.noreply.github.com>
2024-08-16 08:58:46 -0700

    finish program5

--- Program5.java
+++ Program5.java
@@ -1,9 +1,23 @@
 //import stuff here?
 
 //Your code here
-
+public class Program5{
+    public static void main(String args[]){
+        String names[] = new String[] {"Royale", "Koopa King", "Pipe Frame", "Badwagon"};
+        int miles[] = new int[] {286, 412, 361, 161};
+        int gallons[] = new int[] {9, 40, 18, 11};
+        for (int i=0; i<4; i++){
+            System.out.println(names[i]+ " averaged " + (double) miles[i]/gallons[i] + "m/g.");
+        }
+        
+    }
+}
 //Paste console output below:
 /*
+Royale averaged 31.77777777777778m/g.
+Koopa King averaged 10.3m/g.
+Pipe Frame averaged 20.055555555555557m/g.
+Badwagon averaged 14.636363636363637m/g.
 
 
 */
